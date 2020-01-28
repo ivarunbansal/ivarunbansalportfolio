9f3b82296dedff51577f8beb6bd4245517eb7f31
VarunBansal <vbansal@xebia.com>
2020-01-28 16:42:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from  './components/home';
+import About from  './components/about';
+import Contact from  './components/contact';
+import Education from  './components/education';
+import Skills from  './components/skills';
+import Sidebar from  './components/sidebar';
+import Timeline from './components/timeline'
+import { Experience } from './components/experience';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Home></Home>
+					<About></About>
+         <Education></Education>
+            <Skills></Skills>
+					<Experience></Experience>
+          <Contact></Contact>
+          	</div>
+      	</div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/timeline.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component{
+
+render(){
+return(
+
+<div></div>
+
+
+)
+
+
+}
+
+
+}
