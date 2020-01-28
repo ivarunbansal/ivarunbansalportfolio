96e88359613841efb7dd3c162a1471f60b455a8c
VarunBansal <vbansal@xebia.com>
2020-01-28 16:53:57 +0530

    Updates

---
+++ precache-manifest.f82c2d3e99cff3f18c1d594e74180cf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d423fbc7a674bb49276e7af0723fbc30",
+    "url": "/ivarunbansalportfolio/index.html"
+  },
+  {
+    "revision": "7ca1ebe05023c76dccd9",
+    "url": "/ivarunbansalportfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "73fcc203ab8441dea04a",
+    "url": "/ivarunbansalportfolio/static/js/2.6987f592.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ivarunbansalportfolio/static/js/2.6987f592.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ca1ebe05023c76dccd9",
+    "url": "/ivarunbansalportfolio/static/js/main.d03ad64a.chunk.js"
+  },
+  {
+    "revision": "5d551a40bc487e9355e0",
+    "url": "/ivarunbansalportfolio/static/js/runtime-main.e6164ead.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ivarunbansalportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
